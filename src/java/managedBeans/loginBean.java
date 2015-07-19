4ce4e0eeb3d723cddaa370a8fa2963e43cded1b5
Cy <Cy@141.47.152.35>
2015-07-19 12:01:47 +0200

    Layout Verbesserungen (xhtml), Logout Button, Exception-Handling

--- src/java/managedBeans/loginBean.java
+++ src/java/managedBeans/loginBean.java
@@ -14,6 +14,7 @@ import fachklassen.Student;
 import java.io.Serializable;
 import java.util.ArrayList;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
@@ -26,17 +27,17 @@ import javax.faces.context.FacesContext;
  */
 @ManagedBean
 @SessionScoped
-public class loginBean implements Serializable{
+public class loginBean implements Serializable {
+
     @EJB
     private LoginHandler loginHandler;
     @EJB
     private LAHandler lAHandler;
-    
+
     private Student student;
     private Antragsposition antragsposition;
     private LearningAgreement la;
-    
-    
+
     public Antragsposition getAntragsposition() {
         return antragsposition;
     }
@@ -52,14 +53,12 @@ public class loginBean implements Serializable{
     public void setLa(LearningAgreement la) {
         this.la = la;
     }
- 
-      
+
     public loginBean() {
-        student=new Student();
+        student = new Student();
         la = new LearningAgreement();
-      
+
     }
-    
 
     public Student getStudent() {
         return student;
@@ -69,35 +68,56 @@ public class loginBean implements Serializable{
         this.student = student;
     }
 
-          
-    public String login (){
-        System.out.print(student.getBenutzername()+student.getPasswort());
-        student=loginHandler.login(student.getBenutzername(),student.getPasswort());
-        System.out.println(student.getBenutzername()+student.getNachname());
+//    public String login (){
+//        System.out.print(student.getBenutzername()+student.getPasswort());
+//        student=loginHandler.login(student.getBenutzername(),student.getPasswort());
+//        System.out.println(student.getBenutzername()+student.getNachname());
+//        return "home.xhtml";
+//    }
+    public String login() throws Exception {
+        this.student = loginHandler.login(this.student.getBenutzername(), this.student.getPasswort());
+        if (this.student != null) {
+            return "home.xhtml";
+        } else {
+            FacesContext.getCurrentInstance().addMessage("login", new FacesMessage(FacesMessage.SEVERITY_WARN, "Falsche Benutzerdaten. Bitte überprüfen Sie Ihre Zugangsdaten", "Bitte überprüfen Sie Ihre Zugangsdaten"));
+            FacesContext facesContext = FacesContext.getCurrentInstance();
+            facesContext.getExternalContext().invalidateSession();
+            return "login.xhtml";
+        }
+    }
+
+    public String weiterleitungLA(Antragsposition a) {
+        antragsposition = a;
+        if (antragsposition.getStatusAntragsposition().equals("genehmigt")) {
+            if (antragsposition.getLearningAgreement1() != null) {
+                la = antragsposition.getLearningAgreement1();
+            } else {
+                la.setAntragsposition1(antragsposition);
+            //antragsposition.setLearningAgreement1(la);
+                //lAHandler.speichereAntragsposition(antragsposition);
+                lAHandler.speichereNeuesLA(la);
+
+            }
+            return "editLA.xhtml";
+        } else {
+            FacesContext.getCurrentInstance().addMessage("editLA", new FacesMessage(FacesMessage.SEVERITY_WARN, "Ihr Antrag ist noch nicht genehmigt. Sie können noch kein LA anlegen", "Warten Sie auf Genehmigung"));
+        }
         return "home.xhtml";
     }
-    
-     public String weiterleitungLA(Antragsposition a){
-        antragsposition=a;
-       if (antragsposition.getLearningAgreement1()!=null){
-            la=antragsposition.getLearningAgreement1();
-        }
-        else{
-            la.setAntragsposition1(antragsposition);
-            //antragsposition.setLearningAgreement1(la);
-            //lAHandler.speichereAntragsposition(antragsposition);
-            lAHandler.speichereNeuesLA(la);
-            
-        }
-        return "editLA.xhtml";
-    }
-   public String positionLoeschen (LearningAgreementPosition lap){
-       lAHandler.loescheLAPosition(lap);
+
+    public String positionLoeschen(LearningAgreementPosition lap) {
+        lAHandler.loescheLAPosition(lap);
        //LearningAgreement la2 =lAHandler.findeLA(lap.getLearningAgreement1().getLearningAgreementNummer());
-       //la=la2;
-       la.getLearningAgreementPosition1().remove(lap);
-       FacesContext.getCurrentInstance().renderResponse();
+        //la=la2;
+        la.getLearningAgreementPosition1().remove(lap);
+        FacesContext.getCurrentInstance().renderResponse();
         return "editLA.xhtml";
     }
-   
+
+    public void userLogout() throws Exception {
+        student = null;
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        facesContext.getExternalContext().invalidateSession();
+        facesContext.getExternalContext().redirect("login.xhtml");
+    }
 }
